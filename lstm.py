88bced3bfae30b1af918c96e86d94653f566743b
John Wohlbier <jgwohlbier@sei.cmu.edu>
2020-01-10 12:54:34 -0500

    add code

--- lstm.py
+++ lstm.py
@@ -1,14 +1,20 @@
 #!/usr/bin/env python
 import numpy as np
+import tensorflow as tf
 
 if __name__ == "__main__":
     inputs = np.random.random([32, 10, 8]).astype(np.float32)
-# lstm = tf.keras.layers.LSTM(4)
-# 
-# output = lstm(inputs)  # The output has shape `[32, 4]`.
-# 
-# lstm = tf.keras.layers.LSTM(4, return_sequences=True, return_state=True)
-# 
-# # whole_sequence_output has shape `[32, 10, 4]`.
-# # final_memory_state and final_carry_state both have shape `[32, 4]`.
-# whole_sequence_output, final_memory_state, final_carry_state = lstm(inputs)
+    lstm = tf.keras.layers.LSTM(4)
+ 
+    output = lstm(inputs)  # The output has shape `[32, 4]`.
+
+    lstm = tf.keras.layers.LSTM(4, return_sequences=True, return_state=True)
+
+    # whole_sequence_output has shape `[32, 10, 4]`.
+    # final_memory_state and final_carry_state both have shape `[32, 4]`.
+    whole_sequence_output, final_memory_state, final_carry_state = lstm(inputs)
+
+    print(inputs)
+    print(whole_sequence_output)
+    print(final_memory_state)
+    print(final_carry_state)

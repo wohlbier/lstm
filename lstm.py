f59b8028bf80a5d5f69ab847a4952a52aa1594db
John Wohlbier <jgwohlbier@sei.cmu.edu>
2020-01-15 15:52:34 -0500

    adding notes

--- lstm.py
+++ lstm.py
@@ -3,18 +3,32 @@ import numpy as np
 import tensorflow as tf
 
 if __name__ == "__main__":
+    # data presented with 
+    batch = 32       # number of samples presented to network at a time
+    timesteps = 10   # sequence length
+    feature_size = 8 # feature vector per sample per sequence element
+
+    # [batch, timesteps, feature]
     inputs = np.random.random([32, 10, 8]).astype(np.float32)
-    lstm = tf.keras.layers.LSTM(4)
- 
+
+    # units is number of internal units
+    units = 4
+    lstm = tf.keras.layers.LSTM(units = units)
     output = lstm(inputs)  # The output has shape `[32, 4]`.
 
-    lstm = tf.keras.layers.LSTM(4, return_sequences=True, return_state=True)
+    lstm.summary()
 
-    # whole_sequence_output has shape `[32, 10, 4]`.
-    # final_memory_state and final_carry_state both have shape `[32, 4]`.
-    whole_sequence_output, final_memory_state, final_carry_state = lstm(inputs)
-
+#    lstm = tf.keras.layers.LSTM(4, return_sequences=True, return_state=True)
+#
+#    # whole_sequence_output has shape `[32, 10, 4]`.
+#    # final_memory_state and final_carry_state both have shape `[32, 4]`.
+#    whole_sequence_output, final_memory_state, final_carry_state = lstm(inputs)
+#
     print(inputs)
-    print(whole_sequence_output)
-    print(final_memory_state)
-    print(final_carry_state)
+    print(inputs.shape)
+#    print(whole_sequence_output)
+#    print(whole_sequency_output.shape)
+#    print(final_memory_state)
+#    print(final_memory_state.shape)
+#    print(final_carry_state)
+#    print(final_carry_state.shape)
---
+++ profile.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+nvprof -o out.nvvp python lstm.py

e17aa0d9e662f6ed07b0010bdea8f10548562eed
John Wohlbier <jgwohlbier@sei.cmu.edu>
2020-01-16 09:45:19 -0500

    use functional api to implement lstm

--- lstm.py
+++ lstm.py
@@ -1,34 +1,31 @@
 #!/usr/bin/env python
 import numpy as np
 import tensorflow as tf
+from tensorflow.keras.layers import Input, LSTM
+from tensorflow.keras.models import Model
+from tensorflow.keras.utils import plot_model
 
 if __name__ == "__main__":
-    # data presented with 
+
+    # data specs
     batch = 32       # number of samples presented to network at a time
     timesteps = 10   # sequence length
-    feature_size = 8 # feature vector per sample per sequence element
+    feat_sz = 8      # feature vector per sample per sequence element
+    input = Input(shape=(timesteps,feat_sz))
+
+    # number of internal LSTM units
+    units = 4
+    lstm = LSTM(units=units, \
+                return_sequences=True, return_state=True,)(input)
+    model = Model(inputs=input, outputs=lstm)
+
+    print(model.summary())
+    plot_model(model, to_file='lstm.png')
 
     # [batch, timesteps, feature]
-    inputs = np.random.random([32, 10, 8]).astype(np.float32)
+    inputs = np.random.random([batch, timesteps, feat_sz]).astype(np.float32)
 
-    # units is number of internal units
-    units = 4
-    lstm = tf.keras.layers.LSTM(units = units)
-    output = lstm(inputs)  # The output has shape `[32, 4]`.
-
-    lstm.summary()
-
-#    lstm = tf.keras.layers.LSTM(4, return_sequences=True, return_state=True)
-#
-#    # whole_sequence_output has shape `[32, 10, 4]`.
-#    # final_memory_state and final_carry_state both have shape `[32, 4]`.
-#    whole_sequence_output, final_memory_state, final_carry_state = lstm(inputs)
-#
-    print(inputs)
-    print(inputs.shape)
-#    print(whole_sequence_output)
-#    print(whole_sequency_output.shape)
-#    print(final_memory_state)
-#    print(final_memory_state.shape)
-#    print(final_carry_state)
-#    print(final_carry_state.shape)
+    # whole_sequence_output has shape `[32, 10, 4]`.
+    # final_memory_state and final_carry_state both have shape `[32, 4]`.
+    whole_sequence_output, final_memory_state, final_carry_state \
+        = model(inputs)
